e23a4b009dd54d97cbbc9271ebe997c501229eed
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-01 14:15:33 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -8,4 +8,3 @@ def call(Map config = [:]) {
             listRepo = readJSON(text: sh(returnStdout: true, script: """"aws codeartifact list-package-versions --region us-east-1 --domain spanning --repository shared --package ${config.package} --format maven --namespace com.github.SpanningCloudApps.stitch --max-results 1 --sort-by PUBLISHED_TIME """).trim())
                     }
                 }
-}
