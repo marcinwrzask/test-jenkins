93bee4c018510f6f51dcba2fc0901e45cda09361
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-01 13:11:03 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-     sh "aws codeartifact list-package-versions --domain spanning --repository shared --package ${config.package} --format maven --namespace com.github.SpanningCloudApps.stitch --max-results 1 --sort-by PUBLISHED_TIME "
+     sh "aws codeartifact list-package-versions --region us-east-1 --domain spanning --repository shared --package ${config.package} --format maven --namespace com.github.SpanningCloudApps.stitch --max-results 1 --sort-by PUBLISHED_TIME "
 
 }
 
