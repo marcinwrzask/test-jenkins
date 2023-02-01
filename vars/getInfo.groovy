b6205e5af1f4202b0ef8d737288fe263dff3ac20
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-01 14:42:30 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -14,7 +14,6 @@ def call(Map config = [:]) {
         --format maven \
         --namespace com.github.SpanningCloudApps.stitch \
         --max-results 1 \
-        --sort-by PUBLISHED_TIME").trim()),
+        --sort-by PUBLISHED_TIME").trim())}
         sh 'listRepo[0]'
         }
-    }
