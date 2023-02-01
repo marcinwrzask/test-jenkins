1c95c41242ec12628feb36d83c708d9ed366e594
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-01 13:40:23 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -1,11 +1,20 @@
 def call(Map config = [:]) {
 
+
+// potrzebne zmienne w zależnosci od projektu
+format = 
+namespace = 
+
+
+
     withCredentials([[
     $class: 'AmazonWebServicesCredentialsBinding',
     credentialsId: 'aws-codeartifact',
     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-             sh "aws codeartifact list-package-versions --region us-east-1 --domain spanning --repository shared --package ${config.package} --format maven --namespace com.github.SpanningCloudApps.stitch --max-results 1 --sort-by PUBLISHED_TIME "
+            script_version = []
+            script_version = sh "aws codeartifact list-package-versions --region us-east-1 --domain spanning --repository shared --package ${config.package} --format maven --namespace com.github.SpanningCloudApps.stitch --max-results 1 --sort-by PUBLISHED_TIME "
+            sh "println script_version[0]['versions'][0]['version']"
         }
 }
 
