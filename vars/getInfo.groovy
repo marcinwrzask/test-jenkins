ba3784acf16a25e885307b0bac63061ece991da9
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-02 14:01:58 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -11,7 +11,7 @@ def call(Map config = [:]) {
         --domain spanning \
         --repository shared \
         --output text \
-        --query "packages[?package=='metrics-data'].format" """ )}
+        --query "packages[?package=='${config.packageName}'].format" """ )}
         echo "${config.packageName}"
         echo "${formate}"
         println "${formate}"
