9a56c39430b6c1e5fd9d2c03d01cdbac6c98bae6
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-03 09:44:36 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -39,4 +39,35 @@ def call(Map config = [:]) {
         --query "versions[*].[version]" """).trim() }
         echo "Latest packge version is: ${version}"
     }
+    if (remote) {{
+        format = sh(returnStdout: true, script: """#!/bin/bash
+        aws codeartifact list-packages \
+        --region us-east-1 \
+        --domain spanning \
+        --repository shared \
+        --output text \
+        --query "packages[?package=='${config.packageName}'].format" """).trim()
+
+        namespace = sh(returnStdout: true, script: """#!/bin/bash
+        aws codeartifact list-packages \
+        --region us-east-1 \
+        --domain spanning \
+        --repository shared \
+        --output text \
+        --query "packages[?package=='${config.packageName}'].namespace" """).trim()
+        
+        version = sh(returnStdout: true, script: """#!/bin/bash
+        aws codeartifact list-package-versions \
+        --region us-east-1 \
+        --domain spanning \
+        --repository shared \
+        --package ${config.packageName} \
+        --format ${format} \
+        --namespace ${namespace} \
+        --max-results 1 \
+        --sort-by PUBLISHED_TIME \
+        --output text \
+        --query "versions[*].[version]" """).trim() }
+        echo "Latest packge version is: ${version}"
+    }
 }
