0c98d9bf9b0935bf2aaf11c09a7258da9d718bfc
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-03 12:05:23 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -5,35 +5,36 @@ def call(Map config = [:]) {
 
   def local = true
   def remote = false
-  def package_version_call () {
-      format = sh(returnStdout: true, script: """#!/bin/bash
-      aws codeartifact list-packages \
-      --region us-east-1 \
-      --domain spanning \
-      --repository shared \
-      --output text \
-      --query "packages[?package=='${config.packageName}'].format" """).trim()
+  def packageVersionCall() {
+    format = sh(returnStdout: true, script: """#!/bin/bash
+    aws codeartifact list-packages \
+    --region us-east-1 \
+    --domain spanning \
+    --repository shared \
+    --output text \
+    --query "packages[?package=='${config.packageName}'].format" """).trim()
 
-      namespace = sh(returnStdout: true, script: """#!/bin/bash
-      aws codeartifact list-packages \
-      --region us-east-1 \
-      --domain spanning \
-      --repository shared \
-      --output text \
-      --query "packages[?package=='${config.packageName}'].namespace" """).trim()
-      
-      sh(returnStdout: true, script: """#!/bin/bash
-      aws codeartifact list-package-versions \
-      --region us-east-1 \
-      --domain spanning \
-      --repository shared \
-      --package ${config.packageName} \
-      --format ${format} \
-      --namespace ${namespace} \
-      --max-results 1 \
-      --sort-by PUBLISHED_TIME \
-      --output text \
-      --query "versions[*].[version]" """).trim() }
+    namespace = sh(returnStdout: true, script: """#!/bin/bash
+    aws codeartifact list-packages \
+    --region us-east-1 \
+    --domain spanning \
+    --repository shared \
+    --output text \
+    --query "packages[?package=='${config.packageName}'].namespace" """).trim()
+
+    sh(returnStdout: true, script: """#!/bin/bash
+    aws codeartifact list-package-versions \
+    --region us-east-1 \
+    --domain spanning \
+    --repository shared \
+    --package ${config.packageName} \
+    --format ${format} \
+    --namespace ${namespace} \
+    --max-results 1 \
+    --sort-by PUBLISHED_TIME \
+    --output text \
+    --query "versions[*].[version]" """).trim()
+    }
 
   if (local) {
   // access by credentials
