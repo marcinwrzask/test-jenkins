ddbe206e47f5f878ee98acfdf14f7e6b51cd66cc
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-03 11:59:36 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -1,73 +1,63 @@
 def call(Map config = [:]) {
-    def local = true
-    def remote = false
+  if (config['packageName'] == null) {
+  error(['"packageName" argument is mandatory', help()].join("\n"))
+  }
 
-    if (local) {
-    // access by credentials
-    withCredentials([[
-    $class: 'AmazonWebServicesCredentialsBinding',
-    credentialsId: 'aws-codeartifact',
-    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-        format = sh(returnStdout: true, script: """#!/bin/bash
-        aws codeartifact list-packages \
-        --region us-east-1 \
-        --domain spanning \
-        --repository shared \
-        --output text \
-        --query "packages[?package=='${config.packageName}'].format" """).trim()
+  def local = true
+  def remote = false
+  def package_version {
+      format = sh(returnStdout: true, script: """#!/bin/bash
+      aws codeartifact list-packages \
+      --region us-east-1 \
+      --domain spanning \
+      --repository shared \
+      --output text \
+      --query "packages[?package=='${config.packageName}'].format" """).trim()
 
-        namespace = sh(returnStdout: true, script: """#!/bin/bash
-        aws codeartifact list-packages \
-        --region us-east-1 \
-        --domain spanning \
-        --repository shared \
-        --output text \
-        --query "packages[?package=='${config.packageName}'].namespace" """).trim()
-        
-        version = sh(returnStdout: true, script: """#!/bin/bash
-        aws codeartifact list-package-versions \
-        --region us-east-1 \
-        --domain spanning \
-        --repository shared \
-        --package ${config.packageName} \
-        --format ${format} \
-        --namespace ${namespace} \
-        --max-results 1 \
-        --sort-by PUBLISHED_TIME \
-        --output text \
-        --query "versions[*].[version]" """).trim() }
-        echo "Latest packge version is: ${version}"
-    }
-    if (remote) {
-        format = sh(returnStdout: true, script: """#!/bin/bash
-        aws codeartifact list-packages \
-        --region us-east-1 \
-        --domain spanning \
-        --repository shared \
-        --output text \
-        --query "packages[?package=='${config.packageName}'].format" """).trim()
+      namespace = sh(returnStdout: true, script: """#!/bin/bash
+      aws codeartifact list-packages \
+      --region us-east-1 \
+      --domain spanning \
+      --repository shared \
+      --output text \
+      --query "packages[?package=='${config.packageName}'].namespace" """).trim()
+      
+      sh(returnStdout: true, script: """#!/bin/bash
+      aws codeartifact list-package-versions \
+      --region us-east-1 \
+      --domain spanning \
+      --repository shared \
+      --package ${config.packageName} \
+      --format ${format} \
+      --namespace ${namespace} \
+      --max-results 1 \
+      --sort-by PUBLISHED_TIME \
+      --output text \
+      --query "versions[*].[version]" """).trim() }
 
-        namespace = sh(returnStdout: true, script: """#!/bin/bash
-        aws codeartifact list-packages \
-        --region us-east-1 \
-        --domain spanning \
-        --repository shared \
-        --output text \
-        --query "packages[?package=='${config.packageName}'].namespace" """).trim()
-        
-        version = sh(returnStdout: true, script: """#!/bin/bash
-        aws codeartifact list-package-versions \
-        --region us-east-1 \
-        --domain spanning \
-        --repository shared \
-        --package ${config.packageName} \
-        --format ${format} \
-        --namespace ${namespace} \
-        --max-results 1 \
-        --sort-by PUBLISHED_TIME \
-        --output text \
-        --query "versions[*].[version]" """).trim()
-        echo "Latest packge version is: ${version}"
-    }
+  if (local) {
+  // access by credentials
+  withCredentials([[
+  $class: 'AmazonWebServicesCredentialsBinding',
+  credentialsId: 'aws-codeartifact',
+  accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+  secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) package_version () } 
+
+  if (remote) { package_version() } 
+}
+
+def help() {
+  '''
+--------------------------------------------------
+Help:
+    codeArtifactGetLatestDependencies is shared library provide.
+
+    arguments:
+      - packageName                        - package name which should be selected in pipeline script
+
+    usage:
+      gitDiffBetweenCommits firstCommit: "jg3sj2lab",
+                            secondCommit: "123sjdal"
+--------------------------------------------------
+  '''
 }
