5b293587e5b86bfe1f37b5e90e7b68551c0ab415
Marcin Wrzask <marcin.wrzask@kaseya.com>
2023-02-06 11:39:36 +0100

    fix

--- vars/getInfo.groovy
+++ vars/getInfo.groovy
@@ -8,46 +8,46 @@ def call(Map config = [:]) {
     awsCredendialsID = config['credentialsID']
   }
 
-  def packageVersionCall = {
-    format = sh(returnStdout: true, script:
-      """#!/bin/bash
-      aws codeartifact list-packages \
-      --region us-east-1 \
-      --domain spanning \
-      --repository shared \
-      --output text \
-      --query "packages[?package=='${config.packageName}'].format" """).trim()
-    namespace = sh(returnStdout: true, script:
-      """#!/bin/bash
-      aws codeartifact list-packages \
-      --region us-east-1 \
-      --domain spanning \
-      --repository shared \
-      --output text \
-      --query "packages[?package=='${config.packageName}'].namespace" """).trim()
-    packageVersion = sh(returnStdout: true, script:
-      """#!/bin/bash
-      aws codeartifact list-package-versions \
-      --region us-east-1 \
-      --domain spanning \
-      --repository shared \
-      --package ${config.packageName} \
-      --format ${format} \
-      --namespace ${namespace} \
-      --max-results 1 \
-      --sort-by PUBLISHED_TIME \
-      --output text \
-      --query "versions[*].[version]" """).trim()
-  }
+def packageVersionCall = {
+  format = sh(returnStdout: true, script:
+    """#!/bin/bash
+    aws codeartifact list-packages \
+    --region us-east-1 \
+    --domain spanning \
+    --repository shared \
+    --output text \
+    --query "packages[?package=='${config.packageName}'].format" """).trim()
+  namespace = sh(returnStdout: true, script:
+    """#!/bin/bash
+    aws codeartifact list-packages \
+    --region us-east-1 \
+    --domain spanning \
+    --repository shared \
+    --output text \
+    --query "packages[?package=='${config.packageName}'].namespace" """).trim()
+  packageVersion = sh(returnStdout: true, script:
+    """#!/bin/bash
+    aws codeartifact list-package-versions \
+    --region us-east-1 \
+    --domain spanning \
+    --repository shared \
+    --package ${config.packageName} \
+    --format ${format} \
+    --namespace ${namespace} \
+    --max-results 1 \
+    --sort-by PUBLISHED_TIME \
+    --output text \
+    --query "versions[*].[version]" """).trim()
+}
 
   if (config.useAwsInstanceProfile == false) {
     // access by credentials
-    withCredentials([
+    withCredentials([[
       $class: 'AmazonWebServicesCredentialsBinding',
       credentialsId: awsCredendialsID,
       accessKeyVariable: 'AWS_ACCESS_KEY_ID',
       secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-      ]) {
+      ]]) {
         packageVersionCall()
         }
   } else {
